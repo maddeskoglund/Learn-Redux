9a1eb1f41d201f17df3ef4bcd15abeca963c1ef3
maddeskoglund <madelein.blomster@hotmail.com>
2018-05-06 21:29:22 +0200

    #13
    Displaying the Single Photo Component

---
+++ client/components/Comments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Comments = React.createClass({
+    render() {
+        return (
+            <div className='comments'>
+                I'm the comments!!
+            </div>
+        )
+    }
+});
+
+export default Comments;
